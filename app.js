59090cbf4dc181afeb1a9362726c3a5f6d71f1a9
Ryan Walsh <ryan@thehatrack.net>
2015-08-28 15:09:09 -0600

    get request successful

---
+++ app.js
@@ -0,0 +1,30 @@
+$('#getUsers').on('click', function() {
+	return $.ajax({
+		method: 'GET',
+		url: 'http://reqr.es/api/users?page=1',
+		success: function(res) {
+			console.log(res);
+			insertData(res.data);
+		}
+	})
+});
+
+$('#addUser').on('click', function() {
+	
+});
+
+function insertData(arr) {
+	for (var i = 0; i < arr.length; i++) {
+		$('#userInfo' + (i + 1)).html('<div>' + 
+			'User Info:' + 
+			'<li>' + 
+			'First name: ' + arr[i].first_name + 
+			'</li>' +
+			'<li>' +
+			'Last Name: ' + arr[i].last_name +
+			'</li>' +
+			'<hr>' +
+			'</div>'
+			)
+	}
+}
